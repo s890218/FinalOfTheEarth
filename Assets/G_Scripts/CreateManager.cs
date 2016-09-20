94873e801bebfeb9bc82c19197984e6bd93c1925
s890218 <wl00882105@gmail.com>
2016-09-20 23:46:54 +0800

    開始
    創建專案

---
+++ Assets/G_Scripts/CreateManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateManager : MonoBehaviour {
+
+    public GameObject[] m_ArrayCloneObj = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
